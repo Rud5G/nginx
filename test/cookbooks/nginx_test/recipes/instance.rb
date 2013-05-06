c392e93cddf2a4a3dec8cf786188608f0d007b10
miah <miah@chia-pet.org>
2013-05-06 11:29:28 -0700

    add nginx_config to our test recipe.

--- test/cookbooks/nginx_test/recipes/instance.rb
+++ test/cookbooks/nginx_test/recipes/instance.rb
@@ -6,6 +6,12 @@ execute 'apt-get update' do
   notifies :install, 'package[nginx]', :immediately
 end
 
+nginx_config 'example.com' do
+  template 'example.com.conf.erb'
+  options({
+  })
+end
+
 nginx_instance 'example.com' do
   nginx_bin '/usr/bin/nginx'
   path '/opt'
